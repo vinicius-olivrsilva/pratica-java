ef5c6760d827f0cd71ee41c52f48886607378064
Vinícius Oliveira <vinicius.olivrsilva@gmail.com>
2025-01-13 21:55:24 -0300

    Testando alterar uma terceira branch e comparar a diferença

--- leetcode/ola-leetcode/src/Main.java
+++ leetcode/ola-leetcode/src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, LeetCode!");
+        String a = "Jeangela - branch leetcode";
+        System.out.println(a);
     }
 }
