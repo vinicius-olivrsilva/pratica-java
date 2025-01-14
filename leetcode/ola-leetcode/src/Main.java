bc2c2a219e67a2484698e8b12a73e72fd81ce80c
Vinícius Oliveira <vinicius.olivrsilva@gmail.com>
2025-01-14 17:08:36 -0300

    Testando o git no intelliJ

--- leetcode/ola-leetcode/src/Main.java
+++ leetcode/ola-leetcode/src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        String a = "Jeangela - branch leetcode";
+        String a = "Jeangela - branch master";
         System.out.println(a);
+        System.out.println("Testando o git no intelliJ IDEA");
     }
 }
