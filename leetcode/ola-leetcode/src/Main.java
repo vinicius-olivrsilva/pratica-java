0e463d6d55a6279a3653d7141aa3d183206b94b0
Vinícius Oliveira <vinicius.olivrsilva@gmail.com>
2025-01-17 20:33:07 -0300

    deletando testes

--- leetcode/ola-leetcode/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String a = "Jeangela - branch master";
-        System.out.println(a);
-        System.out.println("Testando o git no intelliJ IDEA");
-    }
-}
